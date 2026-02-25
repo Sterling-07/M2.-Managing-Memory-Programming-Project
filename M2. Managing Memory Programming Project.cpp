86e2ba87fb9e492d9c8a5ff31230190ee0701517
sterl <sterlingrodgers2007@gmail.com>
2026-02-25 15:50:41 -0500

    Create variables and functions, along with getting numbers for the array.

--- M2. Managing Memory Programming Project.cpp
+++ M2. Managing Memory Programming Project.cpp
@@ -4,9 +4,34 @@
 #include <iostream>
 using namespace std;
 
+int* ReverseArray(const int arr[], int size);
+void print(const int arr[], int size);
+
 int main()
+{
+	int size = 7;
+	int* arr = new int[size];
+
+	for (int a = 0; a < size; a++)
+	{
+		cout << "Enter a number: ";
+		cin >> arr[a];
+		cout << endl;
+	}
+
+	delete[] arr;
+
+	return 0;
+}
+
+int* ReverseArray(const int arr[], int size)
+{
+
+
+	return nullptr;
+}
+
+void print(const int arr[], int size)
 {
 
 }
-
-
